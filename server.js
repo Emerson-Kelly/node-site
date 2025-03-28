7f156d12a56aa49fe9eb7a8c2b9a9980ff967ce1
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2025-03-27 22:31:45 -0400

    Add 404 Error Handling And Hompage URL Redirect

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var site = "/site"
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var site = "/site";
 
+app.get('/', function(req, res){
+    res.redirect('/index.html');
+});
 
-app.get('*', function (req, res) {
+app.get("*", function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function (err, data) {
+    if (err) {
+      console.log(err);
+      res.writeHead(404, { "Content-Type": "text/html" });
 
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-      fs.readFile(filename, function(err, data) {
-      if (err) {
-        console.log(err);
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      }  
-      res.writeHead(200, {'Content-Type': 'text/html'});
-        res.sendFile(filename, { root: __dirname + site } );
-        res.write(data);
-      return res.end();
-    });
-  })
-  
-  var server = app.listen(port, function () {
-      console.log('Running on port ' + port);
-  })
+      return res.end(`<h1>404 Error Not Found</h1>`);
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.sendFile(filename, { root: __dirname + site });
+    res.write(data);
+    return res.end();
+  });
+});
+
+var server = app.listen(port, function () {
+  console.log("Running on port " + port);
+});
