fd5b6f745009c6e80bd76c23f009b299857be7eb
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2025-03-27 22:02:43 -0400

    Add URL Module

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var site = "/site"
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+
+app.get('*', function (req, res) {
+
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+      fs.readFile(filename, function(err, data) {
+      if (err) {
+        console.log(err);
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      }  
+      res.writeHead(200, {'Content-Type': 'text/html'});
+        res.sendFile(filename, { root: __dirname + site } );
+        res.write(data);
+      return res.end();
+    });
+  })
+  
+  var server = app.listen(port, function () {
+      console.log('Running on port ' + port);
+  })
